7cd8fe3d42a0ab09acafef2ceb219ab8cf8c4afe
Stanislav Yotov <stanislav.yotov@klarrio.com>
2018-12-28 01:51:49 +0100

    trim 09

--- day09/day09.go
+++ day09/day09.go
@@ -5,81 +5,6 @@ import (
 	"fmt"
 )
 
-type player struct {
-	id    int
-	score int
-}
-
-func createMarbles(Nmarbles int) *ring.Ring {
-	marbles := ring.New(Nmarbles)
-	for i := 0; i < marbles.Len(); i++ {
-		marbles.Value = i
-		marbles = marbles.Next()
-	}
-	return marbles.Prev()
-}
-
-func createPlayers(Nplayers int) *ring.Ring {
-	players := ring.New(Nplayers)
-	for i := 0; i < players.Len(); i++ {
-		players.Value = player{id: i}
-		players = players.Next()
-	}
-	return players.Prev()
-}
-
-func findWinner(players *ring.Ring) player {
-	maxScore := 0
-	var winner player
-	for i := 0; i < players.Len(); i++ {
-		score := players.Value.(player).score
-		if score > maxScore {
-			maxScore = score
-			winner = players.Value.(player)
-		}
-		players = players.Next()
-
-	}
-	return winner
-}
-
-// MarbleGame plays a game of marbles with N players and N marbles
-// returns the score of the game
-func MarbleGame(Nplayers, LastMarble int) int {
-	skip := 23
-	nextValue := 23
-	players := createPlayers(Nplayers)
-	marbles := createMarbles(LastMarble + 1)
-
-	circle := marbles.Unlink(1)
-
-	for i := 1; i <= LastMarble; i++ {
-		if i == nextValue {
-			p := players.Value.(player)
-			p.score += i
-			nextValue += skip
-			//
-			marbles.Unlink(1)
-			//
-			circle = circle.Move(-8)
-			p.score += circle.Unlink(1).Value.(int)
-			circle = circle.Move(1)
-			players.Value = p
-		} else {
-			circle = circle.Move(1)
-			circle.Link(marbles.Unlink(1))
-			circle = circle.Move(1)
-		}
-		players = players.Move(1)
-	}
-
-	return findWinner(players).score
-}
-
-type circle struct {
-	position int
-}
-
 // MarbleGameV2 plays a game of marbles with N players and N marbles
 // returns the score of the game faster vestion of MarbleGame
 func MarbleGameV2(Nplayers, LastMarble int) int {
@@ -114,13 +39,6 @@ func max(s []int) int {
 		}
 	}
 	return m
-}
-
-// Run0 task A for current day
-func Run0(players, marbles int) int {
-	score := MarbleGame(players, marbles)
-	fmt.Printf("Day 09 t1: '%v'\n", score)
-	return score
 }
 
 // Run1 task A for current day
